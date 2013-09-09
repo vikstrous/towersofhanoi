2ca37d3e81e8fa6c3d4b4502f193276ec796f2b8
Linda Xie <lindaxie93@gmail.com>
2013-09-09 00:41:01 -0400

    found min alternative for moving to target position

--- towers.py
+++ towers.py
@@ -1,3 +1,5 @@
+import copy 
+
 n,k = map(lambda x: int(x), raw_input().split(' '))
 initial = map(lambda x: int(x), raw_input().split(' '))
 final = map(lambda x: int(x), raw_input().split(' '))
@@ -10,24 +12,26 @@ for i in range(n):
 for i in range(k):
 	stacks[i].reverse() 
 
-def moveToBottom(disk, src, dst, moves):	
+def moveToBottom(disk, src, dst, moves, stacks):	
+	# print disk, src, dst, moves, stacks
+	moves = copy.deepcopy(moves)
+	stacks = copy.deepcopy(stacks)
 	if stacks[src].index(disk) == len(stacks[src])-1 and (len(stacks[dst]) ==0 or disk < stacks[dst(-1)]):
 		moves.append((src, dst))
 		stacks[dst].append(stacks[src].pop())	
-		return moves	
+		return (moves, stacks)
 	else:
 		if len(stacks[dst]) == 0: 
 			nextIndex = stacks[src].index(disk) + 1
 			nextNum = stacks[src][nextIndex]
-			destinations = list(set(range(k)) - set([src, dst]))
-			moves = moveToBottom(nextNum, src, destinations[0], moves)
+			moves, stacks = min(map(lambda dstNum: moveToBottom(nextNum, src, dstNum, moves, stacks), set(range(k)) - set([src, dst])), key=lambda (moves, stacks): len(moves)) 
 		 
-			moves = moveToBottom(disk, src, dst, moves)
-			return moves
+			moves, stacks = moveToBottom(disk, src, dst, moves, stacks)
+			return (moves, stacks)
 
 		else:	 
 			raise Exception( "Can't move %s %s %s index: %s" % (disk, src, dst, stacks[src].index(disk)))
 
 print stacks
-print moveToBottom(disk = 2, src = 0, dst = 2, moves = [])
-print stacks 
+print moveToBottom(disk = 2, src = 0, dst = 2, moves = [], stacks = stacks)
+
